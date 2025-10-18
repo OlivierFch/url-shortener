5dceaf3c8722f6f65c8240943493582755093254
OlivierFch <foucher.olivier@protonmail.com>
2025-10-18 11:27:12 +0200

    add jest config and TU for getSlugLength

---
+++ backend/src/utils/get-slug-length/get-slug-length.test.ts
@@ -0,0 +1,14 @@
+import { getSlugLength } from "./get-slug-length.js";
+
+describe("Unit test for getSlugLength", () => {
+    test.each<{ attempt: number, expectedLength: number }>([
+        { attempt: 1, expectedLength: 6 },
+        { attempt: 2, expectedLength: 6 },
+        { attempt: 3, expectedLength: 7 },
+        { attempt: 4, expectedLength: 7 },
+        { attempt: 5, expectedLength: 8 }
+    ])("getSlugLength should return slug length '$expectedLength' on attempt number '$attempt'", ({ attempt, expectedLength }) => {
+        const result = getSlugLength(attempt);
+        expect(result).toStrictEqual(expectedLength);
+    });
+});
